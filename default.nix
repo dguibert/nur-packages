c31ef73e68679e5b0e45461ab46c286fea93068a
David Guibert <david.guibert@gmail.com>
2019-01-02 15:30:12 +0100

    stdenv adapters: init

--- default.nix
+++ default.nix
@@ -13,6 +13,10 @@ rec {
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
+  envs = import ./envs { inherit pkgs; };
+
+  adapters = import ./pkgs/stdenv/adapters.nix pkgs;
+  inherit (adapters) optimizePackage withOpenMP;
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
---
+++ pkgs/stdenv/adapters.nix
@@ -0,0 +1,40 @@
+/* This file extends <nipkg/pkgs/stdenv/adapters.nix> that contains various functions
+   that take a stdenv and return a new stdenv with different behaviour, e.g. using a different
+   C compiler or customized flags.
+ */
+pkgs:
+
+rec {
+  # https://stackoverflow.com/questions/42136197/how-to-override-compile-flags-for-a-single-package-in-nixos
+  # https://github.com/NixOS/nixpkgs/issues/305
+  extraNativeCflagsFun = pkgs: import (pkgs.runCommand "cflags" {
+     preferLocalBuild = true;
+     #__noChroot = true; # '__noChroot' set, but that's not allowed when 'sandbox' is 'true'
+     #hashChangingValue = builtins.readFile /some/system-dependent-file-that-doesn't-have-size-0 or builtins.currentTime;
+     hashChangingValue = builtins.currentTime;
+     buildInputs = [ pkgs.gcc ];
+   } ''
+   mkdir $out
+   echo "" | gcc -O3 -march=native -mtune=native -v -E - 2>&1 |grep cc1 |sed -r 's/.*? - -(.*)$/-\1/' > $out/flags
+   echo "builtins.readFile ./flags" > $out/default.nix
+  '');
+
+  customFlags = {...}@flags_: customFlagsFun pkgs flags_;
+  customFlagsFun = pkgs:
+    {    flags ? "" # all flags (cflags, fflags, ldflags)
+      , cflags ? ""
+      , fflags ? ""
+      ,ldflags ? ""
+    }:
+    pkg: pkgs.lib.overrideDerivation pkg (attrs: {
+    preConfigure = ''
+      export CFLAGS="$CFLAGS ${flags} ${cflags}"
+      export FFLAGS="$FFLAGS ${flags} ${fflags}"
+      export LDFLAGS="$LDFLAGS ${flags} ${ldflags}"
+    '' + "${attrs.preConfigure or ""}";
+  });
+
+  # newStdenv = stdenv: stdenv // (mkDerivation = args: stdenv.mkDerivation (args // {}));
+  optimizePackage = customFlagsFun pkgs { cflags="${extraNativeCflagsFun pkgs}";};
+  withOpenMP = customFlagsFun pkgs { flags="-fopenmp"; };
+}
