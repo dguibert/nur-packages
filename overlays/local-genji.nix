8ff66f0609f3091132708465e06224b71330a2cb
David Guibert <david.guibert@gmail.com>
2019-02-19 15:26:52 +0100

    go overrides on some local machines

--- overlays/local-genji.nix
+++ overlays/local-genji.nix
@@ -1,3 +1,28 @@
 self: super:
 {
-}
+  go_bootstrap = super.go_bootstrap.overrideAttrs (attrs: {
+    doCheck = false;
+    installPhase = ''
+      mkdir -p "$out/bin"
+      export GOROOT="$(pwd)/"
+      export GOBIN="$out/bin"
+      export PATH="$GOBIN:$PATH"
+      cd ./src
+      ./make.bash
+    '';
+  });
+   go_1_10 = super.go_1_10.overrideAttrs (attrs: {
+    doCheck = false;
+    installPhase = ''
+      mkdir -p "$out/bin"
+      export GOROOT="$(pwd)/"
+      export GOBIN="$out/bin"
+      export PATH="$GOBIN:$PATH"
+      cd ./src
+      ./make.bash
+    '';
+  });
+  go_1_11 = super.go_1_11.overrideAttrs (attrs: {
+    doCheck = false;
+  });
+ }
--- overlays/local-inti.nix
+++ overlays/local-inti.nix
@@ -1,7 +1,26 @@
 self: super:
 {
+  aws-sdk-cpp = super.aws-sdk-cpp.overrideAttrs (attrs: {
+    doCheck = false;
+  });
   boehmgc = super.boehmgc.overrideAttrs (attrs: {
     doCheck = false;
+  });
+  go_1_10 = super.go_1_10.overrideAttrs (attrs: {
+    doCheck = false;
+    installPhase = ''
+      mkdir -p "$out/bin"
+      export GOROOT="$(pwd)/"
+      export GOBIN="$out/bin"
+      export PATH="$GOBIN:$PATH"
+      cd ./src
+      ./make.bash
+    '';
+  });
+  go_1_11 = super.go_1_11.overrideAttrs (attrs: {
+    doCheck = false;
+    GOMAXPROCS=1;
+    enableParallelBuilding = false;
   });
   jemalloc = super.jemalloc.overrideAttrs (attrs: {
     doCheck = false;
