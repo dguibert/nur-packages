a395245ff21c14879e77fe2652933594541c098a
David Guibert <david.guibert@gmail.com>
2019-02-28 18:09:53 +0100

    pgi: init 18.10

---
+++ overlays/pgi-overlay/default.nix
@@ -0,0 +1,46 @@
+self: super:
+let
+  wrapCCWith = with super; { cc
+    , # This should be the only bintools runtime dep with this sort of logic. The
+      # Others should instead delegate to the next stage's choice with
+      # `targetPackages.stdenv.cc.bintools`. This one is different just to
+      # provide the default choice, avoiding infinite recursion.
+      bintools ? if targetPlatform.isDarwin then darwin.binutils else binutils
+    , libc ? bintools.libc
+    , ...
+    } @ extraArgs:
+      callPackage ./build-support/cc-wrapper (let self = {
+    nativeTools = targetPlatform == hostPlatform && stdenv.cc.nativeTools or false;
+    nativeLibc = targetPlatform == hostPlatform && stdenv.cc.nativeLibc or false;
+    nativePrefix = stdenv.cc.nativePrefix or "";
+    noLibc = !self.nativeLibc && (self.libc == null);
+
+    isGNU = cc.isGNU or false;
+    isClang = cc.isClang or false;
+
+    inherit cc bintools libc;
+  } // extraArgs; in self);
+
+  pgiPackages = { version, sha256 }: rec {
+    unwrapped = super.callPackage ./pgi {
+      inherit version sha256;
+    };
+    pgi = wrapCCWith rec {
+      cc = unwrapped;
+      extraPackages = [
+      ];
+      extraBuildCommands = ''
+      ccLDFlags+=" -L${super.numactl}/lib -rpath,${super.numactl}/lib"
+      echo "$ccLDFlags" > $out/nix-support/cc-ldflags
+      '';
+    };
+  };
+
+in
+{
+  pgiPackages_1810 = pgiPackages {
+    version="1810";
+    sha256 ="09p3ndkddvqjxslx8ll93dqyyqwfzxp296fmpq0n1phk58yzhgsb";
+  };
+}
+
