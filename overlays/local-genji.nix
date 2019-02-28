48a7b4a64dd9f4ada2616c83de1cf9cd0099f5a9
David Guibert <david.guibert@gmail.com>
2019-02-28 18:10:54 +0100

    overlays for cluster composed

--- overlays/local-genji.nix
+++ overlays/local-genji.nix
@@ -1,28 +1,34 @@
 self: super:
-{
-  go_bootstrap = super.go_bootstrap.overrideAttrs (attrs: {
-    doCheck = false;
-    installPhase = ''
-      mkdir -p "$out/bin"
-      export GOROOT="$(pwd)/"
-      export GOBIN="$out/bin"
-      export PATH="$GOBIN:$PATH"
-      cd ./src
-      ./make.bash
-    '';
-  });
-   go_1_10 = super.go_1_10.overrideAttrs (attrs: {
-    doCheck = false;
-    installPhase = ''
-      mkdir -p "$out/bin"
-      export GOROOT="$(pwd)/"
-      export GOBIN="$out/bin"
-      export PATH="$GOBIN:$PATH"
-      cd ./src
-      ./make.bash
-    '';
-  });
-  go_1_11 = super.go_1_11.overrideAttrs (attrs: {
-    doCheck = false;
-  });
- }
+let
+  overlay = (super.lib.composeOverlays [
+   (import ./default.nix).default
+   (import ./default.nix).nix-home-nfs-robin-ib-bguibertd
+   (self: super: {
+    go_bootstrap = super.go_bootstrap.overrideAttrs (attrs: {
+      doCheck = false;
+      installPhase = ''
+        mkdir -p "$out/bin"
+        export GOROOT="$(pwd)/"
+        export GOBIN="$out/bin"
+        export PATH="$GOBIN:$PATH"
+        cd ./src
+        ./make.bash
+      '';
+    });
+     go_1_10 = super.go_1_10.overrideAttrs (attrs: {
+      doCheck = false;
+      installPhase = ''
+        mkdir -p "$out/bin"
+        export GOROOT="$(pwd)/"
+        export GOBIN="$out/bin"
+        export PATH="$GOBIN:$PATH"
+        cd ./src
+        ./make.bash
+      '';
+    });
+    go_1_11 = super.go_1_11.overrideAttrs (attrs: {
+      doCheck = false;
+    });
+   })
+  ]);
+in overlay self super
