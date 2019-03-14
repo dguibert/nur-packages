c67638764e00f2390c63b21b1aaaffdd78a7544a
David Guibert <david.guibert@gmail.com>
2019-03-14 17:18:31 +0100

    envs: pass pkgs instead of nixpkgs path

--- default.nix
+++ default.nix
@@ -17,7 +17,7 @@ rec {
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  envs = import ./envs { inherit nixpkgs lib; };
+  envs = import ./envs { inherit pkgs lib; };
 
   adapters = import ./pkgs/stdenv/adapters.nix pkgs;
   inherit (adapters) optimizePackage withOpenMP optimizedStdEnv;
