1475afb75f36fc72e293e44ece71230a91a7af95
David Guibert <david.guibert@gmail.com>
2019-05-10 15:42:27 +0200

    aocc: init 1.3.1

--- overlays/aocc-overlay/default.nix
+++ overlays/aocc-overlay/default.nix
@@ -61,4 +61,11 @@ in
     version="1.3.0";
     sha256 ="0zi1j23h9gmw62d883m3yfa9hjkpznky5jlc4w2d34mmj4njwmms";
   };
+
+  aoccPackages_131 = aoccPackages {
+    release_version = "9.0.0";
+    llvmPackages = super.llvmPackages_8;
+    version="1.3.1";
+    sha256 ="1nbzbw1jal4b8nzk0hj3zwalxna34f50j1v5l2aj2yp6aijla20s";
+  };
 }
