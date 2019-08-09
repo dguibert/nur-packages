cf9285bb321cac2707ffd7f63a4c92f092e27554
David Guibert <david.guibert@gmail.com>
2019-08-09 10:29:04 +0200

    jobsets: secrets as path (will be copied to the Nix store!)

--- jobsets.nix
+++ jobsets.nix
@@ -35,7 +35,7 @@
             "inputs": {
                 "src": { "type": "git", "value": "git://github.com/dguibert/nur-packages.git pu", "emailresponsible": false },
                 "nixpkgs": { "type": "git", "value": "git://github.com/dguibert/nixpkgs.git pu", "emailresponsible": false },
-                "secrets": { "type": "string", "value": "/var/lib/hydra/secrets", "emailresponsible": false }
+                "secrets": { "type": "path", "value": "/var/lib/hydra/secrets", "emailresponsible": false }
             }
         }
     }
