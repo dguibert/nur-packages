e7e44c02c65b24c2036b1f8b0fb29b4bbf162748
David Guibert <david.guibert@gmail.com>
2019-08-09 09:01:03 +0200

    hydra: add pu jobset

--- jobsets.nix
+++ jobsets.nix
@@ -17,7 +17,23 @@
             "emailoverride": "",
             "keepnr": 3,
             "inputs": {
-                "src": { "type": "git", "value": "git://github.com/dguibert/nur-packages.git dg/hydra", "emailresponsible": false },
+                "src": { "type": "git", "value": "git://github.com/dguibert/nur-packages.git master", "emailresponsible": false },
+                "nixpkgs": { "type": "git", "value": "git://github.com/dguibert/nixpkgs.git pu", "emailresponsible": false }
+            }
+        },
+        "pu": {
+            "enabled": 1,
+            "hidden": false,
+            "description": "pu",
+            "nixexprinput": "src",
+            "nixexprpath": "release.nix",
+            "checkinterval": 300,
+            "schedulingshares": 100,
+            "enableemail": false,
+            "emailoverride": "",
+            "keepnr": 3,
+            "inputs": {
+                "src": { "type": "git", "value": "git://github.com/dguibert/nur-packages.git pu", "emailresponsible": false },
                 "nixpkgs": { "type": "git", "value": "git://github.com/dguibert/nixpkgs.git pu", "emailresponsible": false }
             }
         }
