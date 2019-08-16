8434b1d595c0e218691245169657082b6aee061f
David Guibert <david.guibert@gmail.com>
2019-08-16 09:00:26 +0200

    dgemm: comments

--- pkgs/test-dgemm/default.nix
+++ pkgs/test-dgemm/default.nix
@@ -4,6 +4,8 @@
 
 stdenv.mkDerivation {
   name = "test-dgemm-0.1";
+  # https://gitlab.com/arm-hpc/packages/wikis/packages/dgemm
+  # http://portal.nersc.gov/project/m888/apex/mt-dgemm_160114.tgz
   src = ./t_dgemm.c;
   phases = [ "installPhase" ];
 
