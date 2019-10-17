2db49f34bdfa178b2364784126dd262c06238171
David Guibert <david.guibert@gmail.com>
2019-10-17 10:31:32 +0200

    openmpi: 4.0.1 -> 4.0.2

--- pkgs/openmpi/default.nix
+++ pkgs/openmpi/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, fetchpatch, gfortran, perl, libnl
-, rdma-core, zlib, numactl, libevent, hwloc
-, openucx ? null
-, libfabric ? null
+, rdma-core, zlib, numactl, libevent, hwloc, pkgsTargetTarget
+, openucx
+, libfabric
 
 # Enable the Sun Grid Engine bindings
 , enableSGE ? false
@@ -9,37 +9,38 @@
 # Enable Slurm/PMI binding
 , enableSlurm ? false
 , slurm
+, pmix
 
 # Pass PATH/LD_LIBRARY_PATH to point to current mpirun by default
 , enablePrefix ? false
 }:
 
 let
-  version = "4.0.1";
+  version = "4.0.2";
 
 in stdenv.mkDerivation rec {
-  name = "openmpi-${version}";
+  pname = "openmpi";
+  inherit version;
 
   src = with stdenv.lib.versions; fetchurl {
-    url = "https://www.open-mpi.org/software/ompi/v${major version}.${minor version}/downloads/${name}.tar.bz2";
-    sha256 = "02cpzcp113gj5hb0j2xc0cqma2fn04i2i0bzf80r71120p9bdryc";
+    url = "https://www.open-mpi.org/software/ompi/v${major version}.${minor version}/downloads/${pname}-${version}.tar.bz2";
+    sha256 = "0ms0zvyxyy3pnx9qwib6zaljyp2b3ixny64xvq3czv3jpr8zf2wh";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "openmpi-mca_btl_vader_component_close-segfault.patch";
-      url = "https://github.com/open-mpi/ompi/pull/6526.patch";
-      sha256 = "0s7ac9rkcj3fi6ampkvy76njlj478yyr4zvypjc7licy6dgr595x";
-    })
-  ];
 
   postPatch = ''
     patchShebangs ./
+
+    # Ensure build is reproducible
+    ts=`date -d @$SOURCE_DATE_EPOCH`
+    sed -i 's/OPAL_CONFIGURE_USER=.*/OPAL_CONFIGURE_USER="nixbld"/' configure
+    sed -i 's/OPAL_CONFIGURE_HOST=.*/OPAL_CONFIGURE_HOST="localhost"/' configure
+    sed -i "s/OPAL_CONFIGURE_DATE=.*/OPAL_CONFIGURE_DATE=\"$ts\"/" configure
+    find -name "Makefile.in" -exec sed -i "s/\`date\`/$ts/" \{} \;
   '';
 
   buildInputs = with stdenv; [ gfortran zlib openucx libfabric ]
     ++ lib.optionals isLinux [ libnl numactl ]
-    ++ lib.optionals enableSlurm [ slurm ]
+    ++ lib.optionals enableSlurm [ slurm pmix ]
     ++ [ libevent hwloc ]
     ++ lib.optional (isLinux || isFreeBSD) rdma-core;
 
@@ -48,7 +49,8 @@ in stdenv.mkDerivation rec {
   configureFlags = with stdenv; [ "--disable-mca-dso" "--with-cma" ]
     ++ lib.optional isLinux  "--with-libnl=${libnl.dev}"
     ++ lib.optional enableSGE "--with-sge"
-    ++ lib.optional enableSlurm "--with-pmi=${slurm.dev} --with-pmi-libdir=${slurm}/lib"
+    #++ lib.optional enableSlurm "--with-pmi=${slurm.dev} --with-pmi-libdir=${slurm}"
+    ++ lib.optional enableSlurm "--with-pmi=${pmix}"
     ++ lib.optional enablePrefix "--enable-mpirun-prefix-by-default"
     ++ lib.optional (openucx != null) "--enable-mca-no-build=btl-uct"
     ##++ [ "--enable-mpi1-compatibility" ] # to avoid porting libraries (https://www.open-mpi.org/faq/?category=mpi-removed)
@@ -73,6 +75,23 @@ in stdenv.mkDerivation rec {
   postInstall = ''
     rm -f $out/lib/*.la
    '';
+
+  postFixup = ''
+    # default compilers should be indentical to the
+    # compilers at build time
+
+    sed -i 's:compiler=.*:compiler=${pkgsTargetTarget.stdenv.cc}/bin/${pkgsTargetTarget.stdenv.cc.targetPrefix}cc:' \
+      $out/share/openmpi/mpicc-wrapper-data.txt
+
+    sed -i 's:compiler=.*:compiler=${pkgsTargetTarget.stdenv.cc}/bin/${pkgsTargetTarget.stdenv.cc.targetPrefix}cc:' \
+       $out/share/openmpi/ortecc-wrapper-data.txt
+
+    sed -i 's:compiler=.*:compiler=${pkgsTargetTarget.stdenv.cc}/bin/${pkgsTargetTarget.stdenv.cc.targetPrefix}c++:' \
+       $out/share/openmpi/mpic++-wrapper-data.txt
+
+    sed -i 's:compiler=.*:compiler=${pkgsTargetTarget.gfortran}/bin/${pkgsTargetTarget.gfortran.targetPrefix}gfortran:'  \
+       $out/share/openmpi/mpifort-wrapper-data.txt
+  '';
 
   doCheck = true;
 
