7e54f8b4691c49eae198a90c5e9e4a3d62d57cf3
David Guibert <david.guibert@gmail.com>
2019-11-05 22:23:38 +0100

    get upgradeOverride/upgradeReplace

--- lib/default.nix
+++ lib/default.nix
@@ -11,5 +11,21 @@ with pkgs.lib; pkgs.lib // rec {
   makeExtensible' = pkgs: list: builtins.foldl' /*op nul list*/
     (o: f: o.extend f) (makeExtensible (self: pkgs)) list;
 
+  upgradeOverride = package: overrides:
+  let
+    upgraded = package.overrideAttrs overrides;
+  in (upgradeReplace package upgraded);
+
+  upgradeReplace = package: upgraded:
+  let
+    upgradedVersion = (builtins.parseDrvName upgraded.name).version;
+    originalVersion =(builtins.parseDrvName package.name).version;
+
+    isDowngrade = (builtins.compareVersions upgradedVersion originalVersion) == -1;
+
+    warn = builtins.trace
+      "Warning: ${package.name} downgraded by overlay with ${upgraded.name}.";
+    pass = x: x;
+  in (if isDowngrade then warn else pass) upgraded;
 }
 
