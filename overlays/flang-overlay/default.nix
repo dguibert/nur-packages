42ad5df8b89c37fc1abde60dc4ffdbacbc1c2418
David Guibert <david.guibert@gmail.com>
2020-01-08 10:02:50 +0100

    flang: add 8 and 9
    WIP: llvm 8 and 9 are not compiling...
    
    builder for '/nix/store/hi60yk03qxzmrm6s9pcvaab3pnhc2219-llvm-8.0.1.drv' failed with exit code 2; last 10 log lines:
                       from /build/source/unittests/Support/YAMLIOTest.cpp:17:
      /build/source/utils/unittest/googletest/include/gtest/internal/gtest-port.h:873:8: note: because 'testing::internal::StartsWithMatcher<std::__cxx11::basic_string<char> >' has user-provided 'void testing::internal::StartsWithMatcher<StringType>::operator=(const testing::internal::StartsWithMatcher<StringType>&) [with StringType = std::__cxx11::basic_string<char>]'
        873 |   void operator=(type const &)
            |        ^~~~~~~~
      /build/source/utils/unittest/googlemock/include/gmock/gmock-matchers.h:1274:3: note: in expansion of macro 'GTEST_DISALLOW_ASSIGN_'
       1274 |   GTEST_DISALLOW_ASSIGN_(StartsWithMatcher);
            |   ^~~~~~~~~~~~~~~~~~~~~~
      [ 97%] Linking CXX executable SupportTests
      [ 97%] Built target SupportTests
      make: *** [Makefile:152: all] Error 2
    cannot build derivation '/nix/store/k478jsvdj4g7xrc21albj6dd9vks6rsd-clang-8.0.1.drv': 1 dependencies couldn't be built
    cannot build derivation '/nix/store/8d5yjdls35680d9c53sbh5s1yzrl4768-flang-8.0.1.drv': 1 dependencies couldn't be built
    cannot build derivation '/nix/store/a46yxqz37csf3vf83qw1il8qv38lfbai-libpgmath-8.0.1.drv': 1 dependencies couldn't be built
    cannot build derivation '/nix/store/04r19mk5rrksax9w0lnmh6c7wjs5qf5d-clang-wrapper-8.0.1.drv': 1 dependencies couldn't be built
    [0 built (1 failed), 0.0 MiB DL]
    error: build of '/nix/store/04r19mk5rrksax9w0lnmh6c7wjs5qf5d-clang-wrapper-8.0.1.drv', '/nix/store/0b27rp5wi2qrbh0nhhzv2hxfhsqgwd6c-flang-8.0.1.drv' failed

--- overlays/flang-overlay/default.nix
+++ overlays/flang-overlay/default.nix
@@ -3,4 +3,6 @@ self: super:
   flangPackages_5 = super.callPackage ./llvm/5 { };
   flangPackages_6 = super.callPackage ./llvm/6 { };
   flangPackages_7 = super.callPackage ./llvm/7 { };
+  flangPackages_8 = super.callPackage ./llvm/8 { };
+  flangPackages_9 = super.callPackage ./llvm/9 { };
 }
