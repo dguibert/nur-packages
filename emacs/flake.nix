ace554b9d76febcad1941757baf17539a0d86160
David Guibert <david.guibert@gmail.com>
2021-03-02 10:51:17 +0100

    emacs: basic config

--- emacs/flake.nix
+++ emacs/flake.nix
@@ -45,71 +45,13 @@
     devShell = pkgs.mkEnv {
       name = "emacs";
       buildInputs = with pkgs; let
-        # https://gist.github.com/grahamc/2daa060dce38ad18ddfa7927e1b1a1b3
-        #emacsPackaging = pkgs.emacs27-nox.pkgs;
-        emacsPackaging = pkgs.emacs27.pkgs;
-
-        my-texlive = with pkgs; (texlive.combine {
-          inherit (texlive) scheme-medium
-            wrapfig
-            capt-of
-          ; });
-
-        emacsWithPackages = emacsPackaging.emacsWithPackages;
-        # nix-env -f "<nixpkgs>" -qaP -A emacsPackages.elpaPackages
-        # nix-env -f "<nixpkgs>" -qaP -A emacsPackages.melpaPackages
-        # nix-env -f "<nixpkgs>" -qaP -A emacsPackages.melpaStablePackages
-        # nix-env -f "<nixpkgs>" -qaP -A emacsPackages.orgPackages
-        my-emacs = emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
-            #magit          # ; Integrate git <C-x g>
-            #zerodark-theme # ; Nicolas' theme
-          ]) ++ (with epkgs.melpaPackages; [
-            #undo-tree      # ; <C-x u> to show the undo tree
-            #zoom-frm       # ; increase/decrease font size for all buffers %lt;C-x C-+>
-          ]) ++ (with epkgs.elpaPackages; [
-            #auctex         # ; LaTeX mode
-            #beacon         # ; highlight my cursor when scrolling
-            #nameless       # ; hide current package name everywhere in elisp code
-          ]) ++ (with epkgs; [
-            use-package
-            use-package-ensure-system-package
-            nix-mode
-            all-the-icons-ivy
-            doom-modeline
-            doom-themes
-
-            which-key
-
-            evil
-            evil-leader
-            evil-collection
-            evil-magit
-            evil-matchit
-            evil-numbers
-            evil-surround
-            evil-visualstar
-
-            direnv
-            notmuch
-
-            ivy
-            org-bullets
-            rainbow-delimiters
-
-            general
-          ]) ++ [
-            pkgs.notmuch   # From main packages set
-            #my-mode
-          ]);
-
       in [
         my-texlive
         my-emacs
       ];
     };
   })) // rec {
-    overlay = final: prev: {
-    };
+    overlay = import ./overlay.nix;
   };
 }
 
