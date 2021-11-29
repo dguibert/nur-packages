a9c6b2a4e9ee456e516c6e3bec415d1b2f54d157
David Guibert <david.guibert@gmail.com>
2021-11-29 15:29:30 +0100

    genji: x86_64 hm

--- hosts/genji/flake.nix
+++ hosts/genji/flake.nix
@@ -16,7 +16,7 @@
 
     base16-nix           = { url  = "github:atpotts/base16-nix"; flake=false; };
     # For accessing `deploy-rs`'s utility Nix functions
-    deploy-rs.url = "github:dguibert/deploy-rs";
+    deploy-rs.url = "github:dguibert/deploy-rs/pu";
     #deploy-rs.inputs.naersk.inputs.nixpkgs.follows = "nixpkgs";
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -149,43 +149,63 @@
       configuration = { lib, ... }: {
         imports = [
          ({ config, pkgs, lib, ...}: {
+           nixpkgs.overlays = [
+             nix.overlay
+             (import ./overlay.nix)
+             (final: prev: {
+               pinentry = prev.pinentry.override { enabledFlavors = [ "curses" "tty" ]; };
+             })
+           ];
+           services.gpg-agent.pinentryFlavor = lib.mkForce "curses";
+           home.packages = with pkgs; [
+             pkgs.nix
+           ];
            programs.bash.enable = true;
            programs.bash.profileExtra = ''
-             source $HOME/.home-$(uname -m)/.profile
+             if [ -e $HOME/.home-$(uname -m)/.profile ]; then
+               source $HOME/.home-$(uname -m)/.profile
+             fi
            '';
            programs.bash.bashrcExtra = ''
-             if [ -n "$HOME" ] && [ -n "$USER" ]; then
+             if [ -e $HOME/.home-$(uname -m) ]; then
+                 if [ -n "$HOME" ] && [ -n "$USER" ]; then
 
-                 # Set up the per-user profile.
-                 # This part should be kept in sync with nixpkgs:nixos/modules/programs/shell.nix
+                     # Set up the per-user profile.
+                     # This part should be kept in sync with nixpkgs:nixos/modules/programs/shell.nix
 
-                 NIX_LINK=$HOME/.home-$(uname -m)/.nix-profile
+                     NIX_LINK=$HOME/.home-$(uname -m)/.nix-profile
 
-                 # Set up environment.
-                 # This part should be kept in sync with nixpkgs:nixos/modules/programs/environment.nix
-                 export NIX_PROFILES="/home_nfs/bguibertd/nix/var/nix/profiles/default $NIX_LINK"
+                     # Set up environment.
+                     # This part should be kept in sync with nixpkgs:nixos/modules/programs/environment.nix
+                     export NIX_PROFILES="/home_nfs/bguibertd/nix/var/nix/profiles/default $NIX_LINK"
 
-                 # Set $NIX_SSL_CERT_FILE so that Nixpkgs applications like curl work.
-                 if [ -e /etc/ssl/certs/ca-certificates.crt ]; then # NixOS, Ubuntu, Debian, Gentoo, Arch
-                      export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-                 elif [ -e /etc/ssl/ca-bundle.pem ]; then # openSUSE Tumbleweed
-                      export NIX_SSL_CERT_FILE=/etc/ssl/ca-bundle.pem
-                 elif [ -e /etc/ssl/certs/ca-bundle.crt ]; then # Old NixOS
-                      export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
-                 elif [ -e /etc/pki/tls/certs/ca-bundle.crt ]; then # Fedora, CentOS
-                      export NIX_SSL_CERT_FILE=/etc/pki/tls/certs/ca-bundle.crt
-                 elif [ -e "$NIX_LINK/etc/ssl/certs/ca-bundle.crt" ]; then # fall back to cacert in Nix profile
-                      export NIX_SSL_CERT_FILE="$NIX_LINK/etc/ssl/certs/ca-bundle.crt"
-                 elif [ -e "$NIX_LINK/etc/ca-bundle.crt" ]; then # old cacert in Nix profile
-                      export NIX_SSL_CERT_FILE="$NIX_LINK/etc/ca-bundle.crt"
+                     # Set $NIX_SSL_CERT_FILE so that Nixpkgs applications like curl work.
+                     if [ -e /etc/ssl/certs/ca-certificates.crt ]; then # NixOS, Ubuntu, Debian, Gentoo, Arch
+                          export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+                     elif [ -e /etc/ssl/ca-bundle.pem ]; then # openSUSE Tumbleweed
+                          export NIX_SSL_CERT_FILE=/etc/ssl/ca-bundle.pem
+                     elif [ -e /etc/ssl/certs/ca-bundle.crt ]; then # Old NixOS
+                          export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+                     elif [ -e /etc/pki/tls/certs/ca-bundle.crt ]; then # Fedora, CentOS
+                          export NIX_SSL_CERT_FILE=/etc/pki/tls/certs/ca-bundle.crt
+                     elif [ -e "$NIX_LINK/etc/ssl/certs/ca-bundle.crt" ]; then # fall back to cacert in Nix profile
+                          export NIX_SSL_CERT_FILE="$NIX_LINK/etc/ssl/certs/ca-bundle.crt"
+                     elif [ -e "$NIX_LINK/etc/ca-bundle.crt" ]; then # old cacert in Nix profile
+                          export NIX_SSL_CERT_FILE="$NIX_LINK/etc/ca-bundle.crt"
+                     fi
+
+                     if [ -n "''${MANPATH-}" ]; then
+                         export MANPATH="$NIX_LINK/share/man:$MANPATH"
+                     fi
+
+                     export PATH="$NIX_LINK/bin:$PATH"
+                     unset NIX_LINK
                  fi
-
-                 if [ -n "''${MANPATH-}" ]; then
-                     export MANPATH="$NIX_LINK/share/man:$MANPATH"
+             else
+                 if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
+                   source $HOME/.nix-profile/etc/profile.d/nix.sh
                  fi
-
-                 export PATH="$NIX_LINK/bin:$PATH"
-                 unset NIX_LINK
+                 export NIX_IGNORE_SYMLINK_STORE=1 # aloy
              fi
 
              export NIX_IGNORE_SYMLINK_STORE=1 # aloy
@@ -304,6 +324,11 @@
       #         "rm -f ~/software; ln -sfd ${profilePath} ~/software";
       #  profilePath = "${self.legacyPackages.x86_64-linux.nixStore}/var/nix/profiles/per-user/bguibertd/software";
       #};
+      profilesOrder = [
+        "hm-bguibertd-x86_64"
+        #"hm-bguibertd-aarch64"
+        "hm-bguibertd"
+      ];
       profiles.hm-bguibertd = {
         user = "bguibertd";
         sshUser = "bguibertd";
@@ -315,7 +340,7 @@
         user = "bguibertd";
         sshUser = "bguibertd";
         path = (nixpkgsFor "x86_64-linux").deploy-rs.lib.activate.custom self.homeConfigurations.x86_64-linux.home-bguibertd-x86_64.activationPackage
-               "env NIX_STATE_DIR=${self.legacyPackages.x86_64-linux.nixStore}/var/nix ./activate";
+               "env NIX_STATE_DIR=${self.legacyPackages.x86_64-linux.nixStore}/var/nix HOME=${self.homeConfigurations.x86_64-linux.home-bguibertd-x86_64.config.home.homeDirectory} ./activate";
         profilePath = "${self.legacyPackages.x86_64-linux.nixStore}/var/nix/profiles/per-user/bguibertd/hm-x86_64";
       };
       #profiles.hm-bguibertd-aarch64 = {
