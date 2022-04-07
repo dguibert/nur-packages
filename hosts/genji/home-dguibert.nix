fe5b0c62ec2331bdbe83733b5bb580d19c551470
David Guibert <david.guibert@gmail.com>
2022-04-07 17:52:48 +0200

    genji: no bash history truncation

--- hosts/genji/home-dguibert.nix
+++ hosts/genji/home-dguibert.nix
@@ -44,15 +44,11 @@ with lib;
   '';
 
 
-  #programs.bash.historySize = 50000;
-  #programs.bash.historyControl = [ "erasedups" "ignoredups" "ignorespace" ];
-  #programs.bash.historyIgnore = [ "ls" "cd" "clear" "[bf]g" ];
-
-  home.sessionVariables.HISTCONTROL="erasedups:ignoredups:ignorespace";
-  home.sessionVariables.HISTFILE="$HOME/.bash_history";
-  home.sessionVariables.HISTFILESIZE="";
-  home.sessionVariables.HISTIGNORE="ls:cd:clear:[bf]g";
-  home.sessionVariables.HISTSIZE="";
+  programs.bash.historySize = -1; # no truncation
+  programs.bash.historyFile = "$HOME/.bash_history";
+  programs.bash.historyFileSize = -1; # no truncation
+  programs.bash.historyControl = [ "erasedups" "ignoredups" "ignorespace" ];
+  programs.bash.historyIgnore = [ "ls" "cd" "clear" "[bf]g" "exit" ];
 
   programs.bash.shellAliases.ls="ls --color";
 
