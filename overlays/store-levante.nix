6cc6d6761e407f16ff5118194793d5791025f2e8
David Guibert <david.guibert@gmail.com>
2023-01-06 13:15:13 +0100

    store-*: add hosts

---
+++ overlays/store-levante.nix
@@ -0,0 +1,5 @@
+final: prev: with final; let
+in builtins.trace "levante overlay" {
+  nixStore = builtins.trace "nixStore=/home/b/b381115/nix" "/home/b/b381115/nix";
+}
+
---
+++ overlays/store-lumi.nix
@@ -0,0 +1,4 @@
+final: prev: with final; let
+in builtins.trace "lumi overlay" {
+  nixStore = builtins.trace "nixStore=/users/dguibert/nix" "/users/dguibert/nix";
+}
