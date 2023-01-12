aaaf6e9283fb0cb59e7b4cb04babc4040584a74d
David Guibert <david.guibert@gmail.com>
2023-01-12 09:58:12 +0100

    upstreamFails: only on not standard store path

--- overlays/cluster/default.nix
+++ overlays/cluster/default.nix
@@ -13,10 +13,10 @@ final: prev: with prev; {
       doCheck = false;
       doInstallCheck = false;
     });
-    upstreamFails = drv: tryUpstream drv (o: {
+    upstreamFails = drv: if ! (nixStore == "/nix") then tryUpstream drv (o: {
       doCheck = false;
       doInstallCheck = false;
-    });
+    }) else drv;
   });
 
   # fix infinite recursion
