fe7f6fa9de5b9068c85859cdbe41b97d12f0d390
David Guibert <david.guibert@gmail.com>
2023-04-11 08:52:50 +0200

    emacs: convert to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,20 +1,25 @@
 {
   description = "A flake for building my NUR packages";
 
+  inputs.emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.emacs-overlay.url    = "github:nix-community/emacs-overlay";
+  inputs.flake-utils.url      = "github:numtide/flake-utils";
+  inputs.nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixpkgs.url          = "github:dguibert/nixpkgs/pu";
   inputs.nix.url              = "github:dguibert/nix/pu";
-  inputs.nix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.flake-utils.url      = "github:numtide/flake-utils";
+
 
   # for overlays/updated-from-flake.nix
-  inputs.dwm-src.url = "github:dguibert/dwm/pu";
-  inputs.dwm-src.flake = false;
-  inputs.st-src.url = "github:dguibert/st/pu";
-  inputs.st-src.flake = false;
-  inputs.dwl-src.url = "github:dguibert/dwl/pu";
   inputs.dwl-src.flake = false;
-  inputs.mako-src.url = "github:emersion/mako/master";
+  inputs.dwl-src.url = "github:dguibert/dwl/pu";
+  inputs.dwm-src.flake = false;
+  inputs.dwm-src.url = "github:dguibert/dwm/pu";
+  inputs.emacs-src.flake = false;
+  inputs.emacs-src.url = "github:emacs-mirror/emacs/emacs-29";
   inputs.mako-src.flake = false;
+  inputs.mako-src.url = "github:emersion/mako/master";
+  inputs.st-src.flake = false;
+  inputs.st-src.url = "github:dguibert/st/pu";
 
   outputs = inputs@{ self, flake-parts, nixpkgs, nix,... }: let
     inherit (self) outputs;
--- modules/flake-parts/nixpkgs.nix
+++ modules/flake-parts/nixpkgs.nix
@@ -6,13 +6,15 @@
           (final: prev: import ../../overlays/default final prev)
           (final: prev: import ../../overlays/extra-builtins final prev)
           (final: prev: import ../../overlays/updated-from-flake.nix final prev)
+          (final: prev: import ../../overlays/emacs.nix final prev)
           inputs.nix.overlays.default
+          inputs.emacs-overlay.overlay
         ];
         config.allowUnfree = true;
         config.allowUnsupportedSystem = true;
     };
 in
-{ 
+{
   perSystem = {config, self', inputs', pkgs, system, ...}: {
     _module.args.pkgs = nixpkgsFor system;
   };
