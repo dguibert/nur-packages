5d9cd61c9a8e2dc4736c7120a69ba8892a2ddf29
David Guibert <david.guibert@gmail.com>
2023-08-04 09:37:40 +0200

    spartan: move store to scratch

--- overlays/store-spartan.nix
+++ overlays/store-spartan.nix
@@ -1,7 +1,8 @@
 final: prev: with final; let
 in builtins.trace "spartan overlay" {
   #nixStore = builtins.trace "nixStore=/home_nfs_robin_ib/bguibertd/nix" "/home_nfs_robin_ib/bguibertd/nix";
-  nixStore = builtins.trace "nixStore=/home_nfs/bguibertd/nix" "/home_nfs/bguibertd/nix";
+  #nixStore = builtins.trace "nixStore=/home_nfs/bguibertd/nix" "/home_nfs/bguibertd/nix";
+  nixStore = builtins.trace "nixStore=/scratch_na/users/bguibertd/nix" "/scratch_na/users/bguibertd/nix";
 
   pythonOverrides = prev.lib.composeOverlays [
     (prev.pythonOverrides or (_:_: {}))
