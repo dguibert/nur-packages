b3a33118d2c6402c488d75666ed1272234fa1e3b
David Guibert <david.guibert@gmail.com>
2023-08-07 22:02:38 +0200

    nixpkgs: restore nixpkgs.nix for spartan

--- modules/flake-parts/nixpkgs.nix
+++ modules/flake-parts/nixpkgs.nix
@@ -3,15 +3,35 @@
       import inputs.nixpkgs {
         inherit system;
         overlays =  [
+          inputs.nix.overlays.default
           (final: prev: import ../../overlays/default final (prev // { inherit inputs; }))
           (final: prev: import ../../overlays/extra-builtins final (prev // { inherit inputs; }))
           (final: prev: import ../../overlays/updated-from-flake.nix final (prev // { inherit inputs; }))
           (final: prev: import ../../overlays/emacs.nix final (prev // { inherit inputs; }))
-          inputs.nix.overlays.default
+          (import ../../overlays/store-spartan.nix)
+          (import ../../overlays/cluster)
           inputs.emacs-overlay.overlay
+          (final: prev: {
+            libuv = prev.libuv.overrideAttrs (o: {
+              doCheck = false;
+              doInstallCheck = false;
+            });
+
+            patchelf = prev.patchelf.overrideAttrs (o: {
+              doCheck = false; # ./replace-add-needed.sh: line 14: ldd: not found
+              doInstallCheck = false;
+            });
+
+            glibcLocales = prev.glibcLocales.overrideAttrs (o: {
+              LOCALEDEF_FLAGS = o.LOCALEDEF_FLAGS ++ [
+                "-c" # https://sourceware.org/bugzilla/show_bug.cgi?id=28845 quiet to generate C.UTF-8
+              ];
+            });
+          })
         ];
         config.allowUnfree = true;
         config.allowUnsupportedSystem = true;
+        config.replaceStdenv = import ../../stdenv.nix;
     };
 in
 {
