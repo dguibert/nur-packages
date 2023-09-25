943cf94ea3314dedf7ca62602d7fa943ba7e129b
David Guibert <david.guibert@gmail.com>
2023-09-25 14:50:06 +0200

    nix #7265: add __structuredAttrs

--- experiments/nix-issues-7265/flake.nix
+++ experiments/nix-issues-7265/flake.nix
@@ -1,4 +1,4 @@
-{  
+{
   outputs = { nixpkgs, ... }: let
     fes = fn: builtins.foldl' ( a: s: a // { ${s} = fn s; } ) {} [
       "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"
@@ -11,6 +11,16 @@
         passAsFile = ["data"];
         buildCommand = ''
           line="$( <"$dataPath"; )";
+          echo "$line" > "$out";
+        '';
+      };
+      struct = nixpkgs.legacyPackages.${system}.stdenv.mkDerivation {
+        name = "structured-attrs";
+        data = builtins.concatStringsSep "" ( builtins.genList toString 1024 );
+        __structuredAttrs = true;
+        passAsFile = ["data"];
+        buildCommand = ''
+          line="$data";
           echo "$line" > "$out";
         '';
       };
