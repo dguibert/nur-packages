1b5aa6cf8d94372d509097c5322825d76a342bd8
David Guibert <david.guibert@gmail.com>
2023-10-31 16:24:06 +0100

    shells/default: put pre-commit

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   description = "A flake for building my NUR packages";
 
   inputs.emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.emacs-overlay.url    = "github:nix-community/emacs-overlay";
-  inputs.flake-utils.url      = "github:numtide/flake-utils";
+  inputs.emacs-overlay.url = "github:nix-community/emacs-overlay";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixpkgs.url          = "github:dguibert/nixpkgs/pu";
-  inputs.nix.url              = "github:dguibert/nix/pu";
-
+  inputs.nixpkgs.url = "github:dguibert/nixpkgs/pu";
+  inputs.nix.url = "github:dguibert/nix/pu";
 
   # for overlays/updated-from-flake.nix
   inputs.dwl-src.flake = false;
@@ -21,42 +20,53 @@
   inputs.st-src.flake = false;
   inputs.st-src.url = "github:dguibert/st/pu";
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, nix,... }: let
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+  inputs.pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    nix,
+    ...
+  }: let
     inherit (self) outputs;
-
-  in flake-parts.lib.mkFlake { inherit inputs; } {
-    flake = {
-      lib = nixpkgs.lib;
-      overlays = import ./overlays { inherit inputs; lib = inputs.nixpkgs.lib; };
-
-      templates = {
-        env_flake = {
-          path = ./templates/env_flake;
-          description = "A bery basic env for my project";
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      flake = {
+        lib = nixpkgs.lib;
+        overlays = import ./overlays {
+          inherit inputs;
+          lib = inputs.nixpkgs.lib;
         };
-        terraform = {
-          path = ./templates/terraform;
-          description = "A template to use terranix/terraform";
+
+        templates = {
+          env_flake = {
+            path = ./templates/env_flake;
+            description = "A bery basic env for my project";
+          };
+          terraform = {
+            path = ./templates/terraform;
+            description = "A template to use terranix/terraform";
+          };
         };
       };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      imports = [
+        #./home/profiles
+        #./hosts
+        ./modules/all-modules.nix
+        #./lib
+        ./apps
+        ./checks
+        ./shells
+      ];
 
+      #perSystem = {config, self', inputs', pkgs, system, ...}: {
+      #};
     };
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    imports = [
-      #./home/profiles
-      #./hosts
-      ./modules/all-modules.nix
-      #./lib
-      ./apps
-      ./checks
-      ./shells
-    ];
-
-    #perSystem = {config, self', inputs', pkgs, system, ...}: {
-    #};
-  };
-
 }
