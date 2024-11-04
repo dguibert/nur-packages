fa69eedbff99ab0d456a7e7f322dc63791bb52a7
David Guibert <david.guibert@gmail.com>
2024-11-04 10:14:59 +0100

    aocc: check 5.0.0

--- checks/aocc.nix
+++ checks/aocc.nix
@@ -15,19 +15,25 @@
   }: let
     pkgs' = import inputs.nixpkgs {
       inherit system;
-      overlays = [
-        self.overlays.aocc-overlay
-      ];
+      overlays =
+        pkgs.overlays
+        ++ [
+          self.overlays.aocc-overlay
+        ];
     };
     aoccPkgs = import inputs.nixpkgs {
       inherit system;
-      config.replaceStdenv = {pkgs, ...}: pkgs'.aoccPackages_320.stdenv;
+      overlays = pkgs.overlays;
+      #config.replaceStdenv = {pkgs, ...}: pkgs'.aoccPackages_320.stdenv;
+      config.replaceStdenv = {pkgs, ...}: pkgs'.aoccPackages_500.stdenv;
     };
   in {
     #checks.aocc-stdenv = pkgs.aoccPackages_500.stdenv;
-    checks.aocc-stdenv = pkgs.aoccPackages_320.stdenv;
-    checks.aocc-zlib = aoccPkgs.zlib;
-    checks.aocc-hello-override = pkgs.hello.override {stdenv = pkgs.aoccPackages_320.stdenv;};
-    checks.aocc-zlib-override = pkgs.zlib.override {stdenv = pkgs.aoccPackages_320.stdenv;};
+    checks.aocc-stdenv = aoccPkgs.stdenv;
+    checks.aocc-hello = aoccPkgs.hello;
+    # TODO why this is needed? otherwise gcc is used
+    checks.aocc-zlib = aoccPkgs.zlib.override {stdenv = aoccPkgs.stdenv;};
+    checks.aocc-hello-override = pkgs.hello.override {stdenv = aoccPkgs.stdenv;};
+    checks.aocc-zlib-override = pkgs.zlib.override {stdenv = aoccPkgs.stdenv;};
   };
 }
